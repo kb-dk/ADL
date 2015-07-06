e61f9bfd2ff1806b4c8798ec6e7c1eebf2dc4723
Ronan McHugh <mchugh.r@gmail.com>
2015-07-06 15:38:54 +0200

    use non-tokenised title for sorting.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -168,7 +168,8 @@ class CatalogController < ApplicationController
     # label in pulldown is followed by the name of the SOLR field to sort by and
     # whether the sort is ascending or descending (it must be asc or desc
     # except in the relevancy case).
-    config.add_sort_field 'score desc', :label => 'relevance'
+    config.add_sort_field 'score desc', :label => 'relevans'
+    config.add_sort_field 'work_title_ssi asc', :label => 'titel'
     # config.add_sort_field 'pub_date_sort desc, title_sort asc', :label => 'year'
     # config.add_sort_field 'author_sort asc, title_sort asc', :label => 'author'
     # config.add_sort_field 'title_sort asc, pub_date_sort desc', :label => 'title'
