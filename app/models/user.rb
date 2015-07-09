1723f71127798f2c5b84db407268c7fd3f2e728d
naderak <nkh@kb.dk>
2015-07-09 12:52:51 +0200

    cas setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,13 @@ class User < ActiveRecord::Base
 
     attr_accessible :email, :password, :password_confirmation
   end
-# Connects this user object to Blacklights Bookmarks. 
+ # Connects this user object to Blacklights Bookmarks.
   include Blacklight::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  #NKH devise :database_authenticatable, :registerable,
+  #NKH       :recoverable, :rememberable, :trackable, :validatable
+  devise :cas_authenticatable
 
   # Method added by Blacklight; Blacklight uses #to_s on your
   # user class to get a user-displayable login/identifier for
