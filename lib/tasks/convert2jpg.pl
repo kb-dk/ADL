8bd524e9ba60729bc88df928011b5a8f1dd84128
naderak <nkh@kb.dk>
2015-07-10 13:36:58 +0200

    Update convert2jpg.pl
    Recursive function.

--- lib/tasks/convert2jpg.pl
+++ lib/tasks/convert2jpg.pl
@@ -2,28 +2,33 @@
 
 use strict;
 
-my $user = "USER_NAME";
-my $server = "SERVER_NAME";
-my $extdir = "EXTERNAL_SERVER_DIRECTORY";
-my $tifdir = "DIRECTORY_FOR_TIFF_FILES";
-my $jpgdir = "DIRECTORY_FOR_JPEG2000_FILE";
-my $kakadubin = "KAKADU_BIN_DIRECTORY";
-my $extension = "JPEG2000_FILE_EXTENSION";
-
-# This will copy all files in external server directory which start with "dalgas200". "$extdir/* will copy all file in 
-# external server directory and "scp -r" will copy all under directory and files in "$extdir".
-system("scp $user\@$server://$extdir/dalgas200*  $tifdir/.");
-
-my @dirlist;
-opendir(DIR,"$tifdir");
-  @dirlist =  grep {-f "$tifdir/$_" } readdir(DIR);
-close DIR;
-
-my @filename;
-if ($#dirlist >= 0) {
-   for (my $z=0;$z<=$#dirlist;$z++){
-      @filename = split(/\./, $dirlist[$z]);  
-      system("$kakadubin/kdu_compress -i $tifdir/$dirlist[$z] -o $jpgdir/$filename[0].$extension -rate 1.0");
-      system("rm -f $tifdir/$dirlist[$z]");
-   }
+my $path1 = "/home/apache_user/chronos-data/adl-facsimiles/adl";                    
+my $path2 = "/home/apache_user/chronos-jp2/adl";                
+my $kakadubin = "/home/apache_user/converter/kakadu-bin";
+my $extension = "jp2";
+ 
+traverse($path1,$path2);
+ 
+sub traverse {
+    my ($file,$jp) = @_;
+ 
+    return if not -d $file;
+    opendir my $dh, $file or die;
+    while (my $sub = readdir $dh) {
+        next if $sub eq '.' or $sub eq '..';
+         
+        system("mkdir $jp/$sub")  unless ((-d "$jp/$sub") || ($sub =~ /(.*)(\.)(tif)/)) ;
+        if ($sub =~ /(.*)(\.)(tif)/){
+            my @filename = split(/\./, $sub);
+            system("$kakadubin/kdu_compress -i $file/$sub -o $jp/$filename[0].$extension -rate 1.0");  
+        }
+        else {
+           traverse("$file/$sub","$jp/$sub");
+        }
+    }
+    close $dh;
+    return;
 }
+
+
+
