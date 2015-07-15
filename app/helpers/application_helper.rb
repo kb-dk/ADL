6ab784f065fe2a97c79e328ba412dc40a4de424d
Ronan McHugh <mchugh.r@gmail.com>
2015-07-15 15:36:55 +0200

    placeholder logic to link page breaks to erez urls

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,4 +37,14 @@ module ApplicationHelper
   def search_type_link(type, label)
     link_to label, '#', data: { search_type: type,  no_turbolink: true }
   end
+
+  # logic to create the url for the ADL facsimiles
+  def img_link(vol_name, pb)
+    pieces = vol_name.partition(/\d.+/)
+    text = pieces.first
+    num = pieces[1].sub('0', '').sub(/[a-zA-Z]+/, '')
+    vol_dir = text + num
+    fname = text[0..3] + num + pb.sub(/[a-zA-Z]/, '')
+    Rails.application.config_for(:adl)['img_url'] % {vol: vol_dir, file: fname}
+  end
 end
