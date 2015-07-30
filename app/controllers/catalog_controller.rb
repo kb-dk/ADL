83c368dfc047f3a6d55fde572dd4b99051636a50
Ronan McHugh <mchugh.r@gmail.com>
2015-07-30 11:30:21 +0200

    remove genre and title field from search interface

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -61,7 +61,6 @@ class CatalogController < ApplicationController
     # :show may be set to false if you don't want the facet to be drawn in the 
     # facet bar
     # config.add_facet_field 'type_ssi', :label => 'Format'
-    config.add_facet_field 'genre_ssi', :label => 'Genre'
     config.add_facet_field 'author_ssi', :label => 'Forfatter', :single => true
     config.add_facet_field 'cat_ssi', :label => 'Kategori'
     # config.add_facet_field 'subject_topic_facet', :label => 'Topic', :limit => 20
@@ -86,10 +85,8 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the index (search results) view
     #   The ordering of the field names is the order of the display 
-    config.add_index_field 'work_title_tesim', :label => 'Title', short_form: true, itemprop: :name
     # config.add_index_field 'title_vern_display', :label => 'Title'
     config.add_index_field 'author_ssi', :label => 'Forfatter', helper_method: :author_link, short_form: true, itemprop: :author
-    config.add_index_field 'cat_ssi', :label => 'Genre', short_form: true, itemprop: :genre
     config.add_index_field 'publisher_ssi', :label => 'Udgivelsesoplysninger', helper_method: :published_fields, short_form: true, itemprop: :publisher
 
     # this adds basic highlighting to index results
@@ -127,7 +124,6 @@ class CatalogController < ApplicationController
 
 
     # Work show fields
-    config.add_show_field 'genre_ssi', :label => 'Type', itemprop: :genre
     config.add_show_field 'author_ssi', :label => 'Forfatter', helper_method: :author_link, itemprop: :author
     config.add_show_field 'publisher_ssi', :label => 'Publisher', itemprop: :publisher
     # don't show the volume field if we're on the landing page for that volume!
