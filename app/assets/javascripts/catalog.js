kb-dk/ADL

commit 0834e85d94812c651f2094bf6bb765b17df3b868

Added scrollsniffer that toggles workheader fixed/static.

app/assets/javascripts/catalog.js
@@ -157,18 +157,22 @@ $(document).ready(function(){
                 ADL.updateBookmarkLink(e);
 
                 // FIXME: Set a class instead, and let the stylesheets do the CSS work!
-                if ($(window).scrollTop() >= 145) {
-                    $('#sidebar').css({
+                if ($(window).scrollTop() >= 55) {
+                    $('.workHeader').css({
                         position: 'fixed',
-                        top: '10px',
-                        right: '0'
+                        top: '50px',
+                        right: '230px', // FIXME: These two numbers is just to emulate a correct markup on my screen - they have got to die!
+                        left: '520px',
+                        paddingBottom: '10px'
                     });
+                    $('.workHeader dl').hide();
                 } else {
-                    $('#sidebar').css({
-                        position: 'relative',
+                    $('.workHeader').css({
+                        position: 'static',
                         top: '0',
                         right: '0'
                     });
+                    $('.workHeader dl').show();
                 }
             },
 
@@ -201,9 +205,8 @@ $(document).ready(function(){
         }
     });
 
-    // setup scrollSniffer that manipulates bookmarks
-    // FIXME: Next line turns on the scrollSniffer - but it does not discriminate between pages (and it should!)
-    // $(window).scroll(ADL.scrollSniffer);
+    // setup scrollsniffer
+    $(window).scroll(ADL.scrollSniffer);
 
     $(document).ajaxComplete(function (e, xhr, options) {
         if (options && options.url && options.url.indexOf('/feedback?') >= 0) { // FIXME: Is this really the best way to pick out the feedback responses?

commit b0f396a37edd19967f7001e05817b06be72c7a07

Bugfix: Fixed the bookmark functionality that broke in 0834e85.
Also outcommented some debris code that related to bookmarking per idm (since
we are not persuing that userstory anymore).

app/assets/javascripts/catalog.js
@@ -139,23 +139,24 @@ $(document).ready(function(){
                 return '';
             },
 
+            /* // FIXME: This shall be incommented if bookmarks should be per paragraph
             updateBookmarkLink: function (e) {
                 // TODO: Somewhere here we need to manipulate the bookmark elements between set and unset!
                 var formElem = $('form.bookmark_toggle'),
                     firstVisibleId = ADL.getFirstVisibleId();
                 if (firstVisibleId.length) {
                     // There is a line id - append it to the bookmark id
-                    formElem.attr('action',
-                    '/bookmarks/' + formElem.attr('data-doc-id') + '%23' + firstVisibleId);
+                    formElem.attr('data-firstVisibleIdm', firstVisibleId);
                 } else {
                     // No line id - set the bookmark to the entire work
-                    formElem.attr('action',
-                        '/bookmarks/' + formElem.attr('data-doc-id'));
+                    formElem.attr('data-firstVisibleIdm', '');
                 }
             },
+            */
 
             scrollSniffer: function (e) {
-                ADL.updateBookmarkLink(e);
+                // ADL.updateBookmarkLink(e); // FIXME: This shall be incommented if bookmarks should be per paragraph
+
                 // FIXME: Set a class instead, and let the stylesheets do the CSS work!
                 if ($(window).scrollTop() >= 55) {
                     $('.workNavbarFixContainer, .workHeaderFixContainer').addClass('fixed');