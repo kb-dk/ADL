2f12431ce0640f9d154beb87cc8557a4023e6758
Polyxeni Makri <poma@kb.dk>
2015-10-01 09:54:59 +0200

    Change facet name from 'Kategori' to 'Genre'

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -64,7 +64,8 @@ class CatalogController < ApplicationController
     # facet bar
     # config.add_facet_field 'type_ssi', :label => 'Format'
     config.add_facet_field 'author_ssi', :label => 'Forfatter', :single => true
-    config.add_facet_field 'cat_ssi', :label => 'Kategori'
+    config.add_facet_field 'cat_ssi', :label => 'Genre'
+
     # config.add_facet_field 'subject_topic_facet', :label => 'Topic', :limit => 20
     # config.add_facet_field 'language_facet', :label => 'Language', :limit => true
     # config.add_facet_field 'lc_1letter_facet', :label => 'Call Number'
