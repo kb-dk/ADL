fff808bc656f9345219b959cd1257905bfe6c40d
Polyxeni Makri <poma@kb.dk>
2015-10-06 14:07:38 +0200

    US1196: facetter skal have "vis alle"-funktion

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -63,7 +63,7 @@ class CatalogController < ApplicationController
     # :show may be set to false if you don't want the facet to be drawn in the 
     # facet bar
     # config.add_facet_field 'type_ssi', :label => 'Format'
-    config.add_facet_field 'author_ssi', :label => 'Forfatter', :single => true
+    config.add_facet_field 'author_ssi', :label => 'Forfatter', :single => true, :limit => 10
     config.add_facet_field 'cat_ssi', :label => 'Genre', helper_method: :translate_model_names
 
     # config.add_facet_field 'subject_topic_facet', :label => 'Topic', :limit => 20
