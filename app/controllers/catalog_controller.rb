01c9e206a4642c616a82aa89b362a85fe146b51e
David Grove Jørgensen <dgj@kb.dk>
2016-09-26 14:48:21 +0200

    updated searchbuilder

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -3,8 +3,6 @@ class CatalogController < ApplicationController
 
   include Blacklight::Catalog
 
-  self.search_params_logic += [:add_work_id]
-
   configure_blacklight do |config|
     ## Default parameters to send to solr for all search-like requests. See also SearchBuilder#processed_parameters
     config.default_solr_params = {
