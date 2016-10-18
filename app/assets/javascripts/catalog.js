77a3252617f7b3c365bc50ec454375e34ef08348
Polyxeni Makri <poma@kb.dk>
2016-10-18 10:25:40 +0200

    Remove unneeded check (if it is text or facsimile)

--- app/assets/javascripts/catalog.js
+++ app/assets/javascripts/catalog.js
@@ -512,8 +512,8 @@ function getURLParameter(url,name) {
 }
 
 // FIXME: When is this called? If it is debris, it should be cleaned up /HAFE
-function index_work_search(volume_id, target_selector, text_label_id){
-    volume_id = encodeURIComponent(volume_id);
+function index_work_search(id, target_selector, text_label_id){
+    id = encodeURIComponent(id);
     qselector = $('#q.search_q.q.form-control');
     q = encodeURIComponent($(qselector).val());
     if (!q.trim()){
@@ -521,7 +521,7 @@ function index_work_search(volume_id, target_selector, text_label_id){
     }else{
         $.ajax({
             type: 'GET',
-            url: '/catalog.json?search_field=leaf&rows=200&sort=position_isi+asc&q='+q+'&workid='+volume_id,
+            url: '/catalog.json?search_field=leaf&rows=200&sort=position_isi+asc&q='+q+'&workid='+id,
             datatype: 'json',
             success: function(data) {
                 $(target_selector).empty();
@@ -531,7 +531,7 @@ function index_work_search(volume_id, target_selector, text_label_id){
                 if (matches_num>0) {
                     $(target_selector).append('<div id="results-header"><p>'+matches_num+' match</p></div>');
                     for (var i= 0; i in docs && i<3; i++) {
-                        $(target_selector).append('<p><a href="/solr_documents/'+volume_id+(ADL.pageType === 'text' ? extractDivId(docs[i].id) : '#' + docs[i].page_id_ssi)+'">'+highlighting[docs[i].id].text_tesim.join("...")+'</a></br>Side: '+docs[i].page_ssi+'</p>');
+                        $(target_selector).append('<p><a href="/solr_documents/'+id+ '#' + docs[i].page_id_ssi+'">'+highlighting[docs[i].id].text_tesim.join("...")+'</a></br>Side: '+docs[i].page_ssi+'</p>');
                     }
                 }else{$(text_label_id).hide();}
             }
